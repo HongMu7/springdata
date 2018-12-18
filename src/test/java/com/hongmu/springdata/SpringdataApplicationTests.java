9509d1d7164f11873cc261015b10ce9c760fd267
罗强 <15700737919@163.com>
2018-12-18 20:52:36 +0800

    start

---
+++ src/test/java/com/hongmu/springdata/SpringdataApplicationTests.java
@@ -0,0 +1,31 @@
+package com.hongmu.springdata;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.SQLException;
+
+@Slf4j
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringdataApplicationTests {
+
+    @Autowired
+    DataSource dataSource;
+
+    @Test
+    public void contextLoads() throws SQLException {
+        System.out.println(dataSource.getClass());
+        Connection connection = dataSource.getConnection();
+        System.out.println(connection);
+        connection.close();
+    }
+
+}
+
